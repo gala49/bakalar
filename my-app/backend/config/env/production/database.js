ea2d7ccf2e92a57a87db9c014c898fa9894ef682
gala49 <lukasgaluska49@gmail.com>
2022-04-26 14:39:34 +0200

    database change

--- my-app/backend/config/env/production/database.js
+++ my-app/backend/config/env/production/database.js
@@ -5,11 +5,11 @@ const config = parse(process.env.DATABASE_URL);
     connection: {
       client: 'postgres',
       connection: {
-        host: ec2-63-32-248-14.eu-west-1.compute.amazonaws.com,
+        host: "ec2-63-32-248-14.eu-west-1.compute.amazonaws.com",
         port: 5432,
-        database: d3qii2evpu21qc,
-        user: nnefynmkrvrobq,
-        password: 2415127c6a3ebb478fd34faf45341be2187e697558b2e9389b2870188c400e57,
+        database: "d3qii2evpu21qc",
+        user: "nnefynmkrvrobq",
+        password: "2415127c6a3ebb478fd34faf45341be2187e697558b2e9389b2870188c400e57",
         ssl: {
           rejectUnauthorized: false
         },
