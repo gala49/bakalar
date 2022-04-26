d18fb478364dd69274b01f94fb4a020a605b5346
gala49 <lukasgaluska49@gmail.com>
2022-04-26 10:49:34 +0200

    added database

---
+++ my-app/backend/env/production/database.js
@@ -0,0 +1,18 @@
+const parse = require('pg-connection-string').parse;
+    const config = parse(process.env.DATABASE_URL);
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: "ec2-63-32-248-14.eu-west-1.compute.amazonaws.com",
+          port: 5432,
+          database: "d3qii2evpu21qc",
+          user: "nnefynmkrvrobq",
+          password: "2415127c6a3ebb478fd34faf45341be2187e697558b2e9389b2870188c400e57",
+          ssl: {
+            rejectUnauthorized: false
+          },
+        },
+        debug: false,
+      },
+    });
