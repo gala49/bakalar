f561f766d03efd34aea39874fa343cabf4bb3f25
gala49 <lukasgaluska49@gmail.com>
2022-04-26 15:14:21 +0200

    server changes

--- my-app/backend/config/env/production/server.js
+++ my-app/backend/config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
     proxy: true,
     url: env('https://flexi-pedikura.herokuapp.com/'),
     app: { 
-      keys: env.array('APP_KEYS')
+      keys: env.array("APP_KEYS", ["testKey1", "testKey2"])
     },
   })
