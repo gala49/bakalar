50316a881388531a8ab38ebd4bd85259d372eafe
gala49 <lukasgaluska49@gmail.com>
2022-04-27 12:30:23 +0200

    keys

--- my-app/backend/config/env/production/server.js
+++ my-app/backend/config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
     proxy: true,
     url: env('https://flexi-pedikura.herokuapp.com/'),
     app: { 
-      keys: env.array("APP_KEYS", ["testKey1", "testKey2"])
+      keys: env.array("APP_KEYS", ["6CeclRvwBc8pA3bkvXLX+g==", "OVcCQAUDpwRnq9VUpl55qw==", "p7Jqd6uG/eBJ+tfviv2CVw==", "Msr/R6uvQscHK8zW75Bi5w=="]),
     },
   })
--- my-app/backend/config/server.js
+++ my-app/backend/config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array("APP_KEYS", ["6CeclRvwBc8pA3bkvXLX+g==", "OVcCQAUDpwRnq9VUpl55qw==", "p7Jqd6uG/eBJ+tfviv2CVw==", "Msr/R6uvQscHK8zW75Bi5w=="]),
   },
 });
